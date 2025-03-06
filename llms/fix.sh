c36375797f0d355688bc7d334056111c2c4fbaf2
yang3kc <yang3kc@gmail.com>
2025-03-06 11:42:40 -0500

    🔄 refactor(script): update execution flow from paste-result to preview-result

--- llms/fix.sh
+++ llms/fix.sh
@@ -2,5 +2,5 @@
 # name: fix
 # interpreter: zsh
 # icon: 🔧
-# after: paste-result
+# after: preview-result
 ~/mambaforge/bin/llm "$POPCLIP_TEXT" --system 'You are a helpful assistant to help me refine the input text. Your job is NOT to answer my question. Instead, you should only return the refined text. Given the input, your priority is to fix any grammar or spelling mistakes. After that, use the existing text as much as possible and try to preserve the style and tone. Below is the transcript. Only return the refined text, nothing else.' -m gpt-4o-mini
